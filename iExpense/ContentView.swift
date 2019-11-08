8edf392409fa1b78163f9b7e6114bf5d71e157a4
Ross <rosko48@gmail.com>
2019-11-08 21:06:26 +0200

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Ross on 11/8/19.
+//  Copyright © 2019 Ross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
